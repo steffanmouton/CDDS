9bc812a2318c5543bb1ccec6e00b90cd2a36c7f8
Steffan Mouton <smouton8993@gmail.com>
2018-10-24 17:26:41 -0500

    Created simple HashTable

---
+++ Hashing/Hashing.h
@@ -0,0 +1,55 @@
+#pragma once
+
+template<typename KeyType, typename T>
+class HashTable
+{
+public:
+	HashTable(unsigned int size)
+		 {
+		m_size = (size);
+		m_data = (new T[size]);
+	}
+
+	~HashTable() { delete[] m_data; }
+
+	unsigned int badHash(KeyType Key)
+	{
+		int key = (int)Key;
+		unsigned int hash = 0;
+		const unsigned int len = m_size;
+
+		for (unsigned int i = 0; i < len; i++)
+		{
+			hash += key++ * len;
+		}
+
+		return hash;
+	}
+
+	T& operator [] (const KeyType& key)
+	{
+		auto hashedKey = badHash(key) % m_size;
+		return m_data[hashedKey];
+	}
+
+	const T& operator [] (const KeyType& key) const
+	{
+		auto hashedKey = badHash(key) % m_size;
+		return m_data[hashedKey];
+	}
+
+	void add(KeyType key, T value)
+	{
+		unsigned int hashedKey = badHash(key);
+		hashedKey %= m_size;
+		m_data[hashedKey] = T(value);
+	}
+
+private:
+	
+
+	T * m_data;
+	unsigned int m_size;
+
+};
+
---
+++ Hashing/Source.cpp
@@ -0,0 +1,44 @@
+#include <functional>
+#include <iostream>
+#include "Hashing.h"
+
+
+namespace HashFunction
+{
+	//implementation of a basic addition hash, def collision-prone
+	
+}
+struct Player
+{
+	////MrMatt gave me this to generate names randomly and print
+	//std::string name;
+	//int health;
+	//friend std::ostream& operator<<(std::ostream& os, const Player& dt)
+	//{
+	//	os << "Player Health: " << dt.health;
+	//	return os;
+	//}
+	
+};
+
+#include <string>
+int main()
+{
+	////MrMatt code:
+	//HashTable<const char*, Player> players(100);
+	//for (int i = 0; i < 25; i++)
+	//{
+	//	auto p = Player();		
+	//	p.name = std::to_string(i).c_str();
+	//	players.add(p.name.c_str(), p);
+	//}
+
+
+	HashTable<const char*, int> test(100);
+	
+	test.add("Steffan", 10);
+ 
+
+
+	std::cout << test["Steffan"];
+}
